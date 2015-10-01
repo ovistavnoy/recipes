180a4a17c172679f29ef92096fd6b0dba61d4fc2
Oleg Vystavnoy <ovistavnoy@dyn.com>
2015-10-01 22:09:02 +0400

    - Очищать форму после добавления рецепта - добавить кнопку добавить в списке рецептов опреденной категории, при нажатии передим на форму добавления с выбранной категорией. - сортировка по имени категорий и рецептов - дата рецепта - кнопка выйти - если нет рецепта у категории - то писать что рецептов нету + кнопка добавить - переносы строк в описании рецепта

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
+jQuery.nl2br = function(varTest){
+    return varTest.replace(/(\r\n|\n\r|\r|\n)/g, "<br>");
+};
+
 (function($) {
     "use strict";
 
     var categories = [
-        {id: 1, name: 'Супы'},
-        {id: 2, name: 'Гарниры'},
-        {id: 3, name: 'Салаты'},
-        {id: 4, name: 'Закуски'},
-        {id: 5, name: 'Десерты'}
+        {id: 1, name: 'Гарниры'},
+        {id: 2, name: 'Десерты'},
+        {id: 3, name: 'Закуски'},
+        {id: 4, name: 'Супы'},
+        {id: 5, name: 'Салаты'},
+        {id: 6, name: 'Напитки'}
     ];
 
     $(document).on("ready", function() {
@@ -21,9 +26,15 @@
                 comment: $('#recipe_comment').val(),
                 used: $('#recipe_used').prop('checked')
             };
-            Recipe.saveRecipe(data, function(){updateListCategories()});
+            Recipe.saveRecipe(data, function(){
+                $('#addRecipeForm').trigger('reset');
+                updateListCategories();
+            });
         });
 
+        $('#exit').on('click', function() {
+            navigator.app.exitApp();
+        })
     });
 
     $( document ).on("deviceready", function(){
@@ -37,6 +48,7 @@
         var SelectHtml = '<option value="">Выберите категорию</option>';
 
         $('#recipe_category').html('');
+        categories.sort(sName);
         for(var i=0; i<categories.length; i++) {
             var count = 0;
             if(countCategories[categories[i].id] != undefined) {
@@ -56,21 +68,52 @@
         var categoryId = $(this).data('id');
         var html = '';
         $('#categoryName').text(getCategoryNameById(categoryId));
+        $('#addRecipe').data('category_id', categoryId);
+        $("a.recipe-add").on("click", addRecipePage);
         var recipes = Recipe.getRecipes(categoryId);
-        for(var i=0; i<recipes.length; i++) {
-            html += '<li><a class="recipe-page" data-id="'+recipes[i].id+'" href="#RecipePage">'+recipes[i].name+'</a></li>';
-        }
-        $('#recipes').html(html);
-        $('#recipes').listview().listview('refresh');
+        if(recipes.length) {
+            $('#emptyRecipes').hide();
+            $('#recipes').show();
+            $('.ui-filterable').show();
+            for(var i=0; i<recipes.length; i++) {
+                html += '<li><a class="recipe-page" data-id="'+recipes[i].id+'" href="#RecipePage">'+recipes[i].name+'</a></li>';
+            }
+            $('#recipes').html(html);
+            $('#recipes').listview().listview('refresh');
 
-        $("a.recipe-page").on("click", recipePage);
+            $("a.recipe-page").on("click", recipePage);
+        } else {
+            $('#emptyRecipes').show();
+            $('#recipes').hide();
+            $('.ui-filterable').hide();
+        }
+
+    }
+
+    function addRecipePage() {
+        var categoryId = $(this).data('category_id');
+        if(categoryId) {
+            $('#recipe_category').val(categoryId);
+            $('#recipe_category').selectmenu('refresh');
+        }
+
     }
 
     function recipePage() {
         var recipe = Recipe.getRecipe($(this).data('id'));
         console.log(recipe);
         $('#recipeName').text(recipe.name);
-        $('#recipeContent').html('<b>Категория:</b> '+getCategoryNameById(recipe.category)+'<br><br><b>Ингридиенты:</b><div>'+recipe.ingredients+'</div><br><b>Описание:</b><div>'+recipe.description+'</div><br><b>Комментарий:</b><div>'+recipe.comment+'</div><br><b>Пробывала?</b> '+(recipe.used ? 'Да' : 'Нет'));
+        var html = '';
+        html += '<b>Категория:</b> '+getCategoryNameById(recipe.category)+'<br><br>';
+        html += '<b>Добавлено:</b> '+getNiceDateTime(recipe.datetime)+'<br><br>';
+        html += '<b>Ингридиенты:</b><div>'+ $.nl2br(recipe.ingredients)+'</div><br>';
+        html += '<b>Описание:</b><div>'+$.nl2br(recipe.description)+'</div><br>';
+        if(recipe.comment) {
+            html += '<b>Комментарий:</b><div>'+$.nl2br(recipe.comment)+'</div><br>';
+        }
+        html += '<b>Пробывала?</b> '+(recipe.used ? 'Да' : 'Нет');
+
+        $('#recipeContent').html(html);
 
     }
 
@@ -83,10 +126,36 @@
         return null;
     }
 
+    function getNiceDateTime(datetime) {
+        var result = '';
+        if(datetime != undefined) {
+            var d = new Date(datetime);
+            result += d.getDate() < 10 ? '0'+d.getDate() : d.getDate();
+            result += '.';
+            result += d.getMonth() < 10 ? '0'+d.getMonth() : d.getMonth();
+            result += '.';
+            result += d.getFullYear();
+            result += ' ';
+            result += d.getHours() < 10 ? '0'+d.getHours() : d.getHours();
+            result += ':';
+            result += d.getMinutes() < 10 ? '0'+d.getMinutes() : d.getMinutes();
+
+        }
+        return result;
+    }
+
 
 }
 )(jQuery);
 
+function sName(i, ii) {
+    if (i.name > ii.name)
+        return 1;
+    else if (i.name < ii.name)
+        return -1;
+    else
+        return 0;
+}
 
 var Recipe = (function(){
 
@@ -98,8 +167,7 @@ var Recipe = (function(){
 
     function _saveRecipe(data, callback) {
         var restoreRecipes = JSON.parse(localStorage.getItem(storageName));
-        console.log(restoreRecipes);
-
+        data.datetime = new Date().getTime();
         if(restoreRecipes != null) {
             data.id = ++restoreRecipes.lastId;
             restoreRecipes.data.push(data);
@@ -122,7 +190,9 @@ var Recipe = (function(){
                     result.push(restoreRecipes.data[i]);
                 }
             }
+            result.sort(sName)
         }
+
         return result;
     }
 
